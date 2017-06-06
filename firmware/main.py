5e3cbf23ff19a34c5550d100aa32e5b00ba72595
HarkonenBade <github@harkonen.net>
2017-06-06 11:10:52 +0100

    Tweak main request routing slightly

--- firmware/main.py
+++ firmware/main.py
@@ -16,16 +16,19 @@ def activate_switch(hold=5000):
 
 
 def handle(addr, meth, path):
-    if path != "/active":
+    if path == "/web":
+        webrepl.start()
+    elif path == "/active":
+        if meth == "GET":
+            rsp_val = str(sw_pin.value() == 1).lower()
+            return 200, '{"active": %s}' % rsp_val
+        elif meth == "PUT":
+            activate_switch()
+            return 200, ""
+        else:
+            return 403, ""
+    else:
         return 404, ""
-    if meth == "GET":
-        rsp_val = str(sw_pin.value() == 1).lower()
-        return 200, '{"active": %s}' % rsp_val
-    elif meth == "PUT":
-        activate_switch()
-        return 200, ""
-    webrepl.start()
-    return 403, ""
 
 
 server.do_http(handle)
